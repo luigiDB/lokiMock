0a2da4edf572ebe101f6dd85440eded358ae6f5f
luigidb <luigi.debianchi@gmail.com>
2023-01-16 13:05:45 +0100

    Initial commit + gradle setup

---
+++ build.gradle.kts
@@ -0,0 +1,133 @@
+
+val ktorVersion = "2.2.2"
+val logbackVersion = "1.2.11"
+val kotlinWrappersVersion = "1.0.0-pre.354"
+val serializationVersion = "1.3.3"
+
+plugins {
+    kotlin("multiplatform") version "1.7.21"
+    application
+}
+
+group = "com.github.luigidb"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven")
+}
+
+kotlin {
+    jvm("server") {
+        withJava()
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
+    js("frontend", IR) {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+    sourceSets {
+//        common
+        val commonMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+//        server
+        val serverMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-netty:2.0.2")
+                implementation("io.ktor:ktor-server-html-builder-jvm:2.0.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+
+                implementation("io.ktor:ktor-serialization:$ktorVersion")
+                implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("io.ktor:ktor-server-cors:$ktorVersion")
+                implementation("io.ktor:ktor-server-compression:$ktorVersion")
+                implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+                implementation("io.ktor:ktor-server-netty:$ktorVersion")
+                implementation("ch.qos.logback:logback-classic:$logbackVersion")
+            }
+        }
+        val serverTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+//        frontend
+        val frontendMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-js:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation(project.dependencies.enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
+                implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion")
+            }
+        }
+        val frontendTest by getting
+    }
+}
+
+application {
+    mainClass.set("com.github.luigidb.application.ServerKt")
+}
+
+tasks.named<Copy>("serverProcessResources") {
+    val frontendBrowserDistribution = tasks.named("frontendBrowserDistribution")
+    from(frontendBrowserDistribution)
+}
+
+tasks.named<JavaExec>("run") {
+    dependsOn(tasks.named<Jar>("serverJar"))
+    classpath(tasks.named<Jar>("serverJar"))
+}
+
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+}
+
+distributions {
+    main {
+        contents {
+            from("$buildDir/libs") {
+                rename("${rootProject.name}-jvm", rootProject.name)
+                into("lib")
+            }
+        }
+    }
+}
+
+//// include JS artifacts in any JAR we generate
+//tasks.getByName<Jar>("jvmJar") {
+//    val taskName = if (project.hasProperty("isProduction")
+//        || project.gradle.startParameter.taskNames.contains("installDist")
+//    ) {
+//        "jsBrowserProductionWebpack"
+//    } else {
+//        "jsBrowserDevelopmentWebpack"
+//    }
+//    val webpackTask = tasks.getByName<org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack>(taskName)
+//    dependsOn(webpackTask) // make sure JS gets compiled first
+//    from(File(webpackTask.destinationDirectory, webpackTask.outputFileName)) // bring output file along into the JAR
+//}
