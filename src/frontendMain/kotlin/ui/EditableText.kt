8d6fc2474a60f9e60a290bf848485ab126411c98
luigidb <luigi.debianchi@gmail.com>
2023-05-06 17:52:38 +0200

    Remove manual handling

--- src/frontendMain/kotlin/ui/EditableText.kt
+++ src/frontendMain/kotlin/ui/EditableText.kt
@@ -1,10 +1,7 @@
 package ui
 
-import org.w3c.dom.HTMLDivElement
 import react.FC
 import react.Props
-import react.dom.events.FormEventHandler
-import react.dom.html.ReactHTML.div
 import react.useState
 import kotlin.js.JSON.stringify
 
@@ -16,22 +13,6 @@ external interface TextProps : Props {
 val editableText = FC<TextProps> { prop ->
 
     val (text, setText) = useState(prop.content)
-
-    val changeHandler: FormEventHandler<HTMLDivElement> = { it ->
-        val content = (it.target as HTMLDivElement).textContent ?: ""
-        println("update on [$text] with [$content]")
-        setText(content)
-        prop.contentUpdate(content)
-    }
-
-    div {
-        contentEditable = true
-
-        onInput = changeHandler
-
-        +text
-    }
-//    <div id="jsoneditor" style="width: 400px; height: 400px;"></div>
 
     JsonEditorModule.JsonEditor {
         value = text
