1072b64efd2ee7744c9a2f815d0b005ea77caad1
luigidb <luigi.debianchi@gmail.com>
2023-05-06 17:53:21 +0200

    Fix Json setup

--- src/frontendMain/kotlin/InitialLokiState.kt
+++ src/frontendMain/kotlin/InitialLokiState.kt
@@ -3,16 +3,16 @@ class InitialLokiState() : LokiState {
     override val endpoint:String = "http://localhost:7777/endpointUnderTest"
     override var request: String = """
         {
-            id: "the id", 
-            name: "name,
-            address: {
-                city: "city",
-                state: "state"
+            "id": "the id", 
+            "name": "name",
+            "address": {
+                "city": "city",
+                "state": "state"
             }
         }
     """.trimIndent()
     override val mocks: List<MockConfigurator> = listOf (
-        MockConfigurator("GET", "endpoint1", "{id : \"request 1\"}", "{id : \"response 1\"}"),
-        MockConfigurator("GET", "endpoint2", "{id : \"request 2\"}", "{id : \"response 2\"}")
+        MockConfigurator("GET", "endpoint1", "{\"id\" : \"request 1\"}", "{\"id\" : \"response 1\"}"),
+        MockConfigurator("GET", "endpoint2", "{\"id\" : \"request 2\"}", "{\"id\" : \"response 2\"}")
     )
 }
