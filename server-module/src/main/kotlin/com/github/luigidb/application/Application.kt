d10c50669c2dcd89f7c0e3661a677ebebb98eb91
luigi <luigi.debianchi@gmail.com>
2023-07-30 16:56:18 +0200

    Restore server plugins

--- server-module/src/main/kotlin/com/github/luigidb/application/Application.kt
+++ server-module/src/main/kotlin/com/github/luigidb/application/Application.kt
@@ -1,8 +1,13 @@
 package com.github.luigidb.application
 
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.compression.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -10,5 +15,21 @@ fun main() {
 }
 
 fun Application.module() {
+    install(ContentNegotiation) {
+        json()
+    }
+
+    install(CORS) {
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Delete)
+        anyHost() //TODO: to be removed
+        allowHeader(HttpHeaders.ContentType)
+    }
+
+    install(Compression) {
+        gzip()
+    }
+
     configureRouting()
 }
+
