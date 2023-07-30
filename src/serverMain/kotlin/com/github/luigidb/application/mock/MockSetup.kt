200fce4c3daec143bd71519017dcaf7f9f386ccb
luigi <luigi.debianchi@gmail.com>
2023-07-30 13:58:39 +0200

    Removed multiplatform kotlin project

--- src/serverMain/kotlin/com/github/luigidb/application/mock/MockSetup.kt
+++
@@ -1,8 +0,0 @@
-package com.github.luigidb.application.mock
-
-interface MockSetup {
-
-    fun addRestMock(httpMethod: String, path: String, request: String, response: String): Boolean
-    fun startMock()
-    fun invalidateMocks()
-}
