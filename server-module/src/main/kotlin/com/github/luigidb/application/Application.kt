f5151cc5a57ae75253b80ca5650c86a95ec890bb
luigi <luigi.debianchi@gmail.com>
2023-08-06 12:10:48 +0200

    Allow local host CORS
    For local debug.
    Run the ktor service and start the frontend with `npm run dev` to develop the frontend without need to restart ktor continuously.

--- server-module/src/main/kotlin/com/github/luigidb/application/Application.kt
+++ server-module/src/main/kotlin/com/github/luigidb/application/Application.kt
@@ -1,5 +1,6 @@
 package com.github.luigidb.application
 
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
@@ -7,6 +8,7 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.plugins.compression.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -16,6 +18,13 @@ fun main() {
 fun Application.module() {
     install(ContentNegotiation) {
         json()
+    }
+
+    install(CORS) {
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Delete)
+        allowHost("127.0.0.1:5173")
+        allowHeader(HttpHeaders.ContentType)
     }
 
     install(Compression) {
